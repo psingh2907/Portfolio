2aba55f75adb1ea1535005388c89075430887589
Praveen Singh <praveen.singh@webmobriltechnologies.com>
2023-07-16 18:41:20 +0530

    Portfolio new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 
-export default function App() {
+
+const App = () => {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
@@ -19,3 +19,5 @@ export default function App() {
     </main>
   );
 }
+
+export default App;
